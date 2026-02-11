900c4c2062a1f582c3db39a60f04c29af50b424a
Jack Fallas <jfallas-2021655@kinal.edu.gt>
2026-02-11 11:54:20 -0600

    HU1 - T3 Configuración de Node y PostgreSQL.

---
+++ configs/db.js
@@ -0,0 +1,50 @@
+`use strict`;
+
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Instancia de Sequelize (Define como nos comunicamos con el sv)
+export const db = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+
+        // Cambiar a 'console.log' si queremos ver el SQL puro en bash
+        logging: false,
+        pool: {
+            // Maximo de conexiones abiertas
+            max: 5,
+            min: 0,
+            // Tiempo maximo para conectar (30s)
+            acquire: 30000,
+            // Tiempo para cerrar una conexion inactiva (10s)
+            idle: 10000
+        }
+    }
+);
+
+// Funcion para probar y sincronizar la conexion
+export const dbConnection = async () => {
+    try {
+        // Autentica la conexion
+        await db.authenticate();
+        console.log('PostgreeSQL | Connection successful.')
+
+        /*
+            Sincronizacion de los modelos:
+            { alter: true} -> Compara el modelo con la tabla y la actualiza si hay cambios.
+             Ideal para desarrollo ya que no borra los datos
+        */
+        await db.sync({ alter: true });
+        console.log('PostgreeSQL | Database synchronized and tables cheked.');
+    } catch (error) {
+        console.error('PostgreSQL | Connectin failed: ', error.message);
+        process.exit(1);
+    }
+};
