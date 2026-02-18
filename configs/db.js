af0f5b393d1e67852d9d2297d353efcd44eb2864
EduardoLG <jlopez-2023086@kinal.edu.gt>
2026-02-18 11:42:26 -0600

    HU2 - T011 Crear contenedor Docker para PostgreSQL y Node

--- configs/db.js
+++ configs/db.js
@@ -12,9 +12,9 @@ export const db = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
+        host: process.env.DB_HOST || 'db',
+        port: process.env.DB_PORT || 5432,
+        dialect: process.env.DB_DIALECT || 'postgres',
 
         // Cambiar a 'console.log' si queremos ver el SQL puro en bash
         // logging: (msg) => logger.info(msg),
